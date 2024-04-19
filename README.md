e1fa3a25cac636eb1e404227d25585dae34f2a34
Fabio Mariantoni <spreadoibaf@gmail.com>
2024-04-19 13:12:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Cerca tutti gli elementi con classe "paragrafo".
+Cerca l' elemento con id "elemento-3"
+Cerca tutti gli elementi h2 della pagina
+
+
---
+++ script.js
@@ -0,0 +1,13 @@
+const html = document.documentElement
+
+const paragrafi = document.querySelectorAll(".paragrafo")
+
+console.log(paragrafi)
+
+const elemento3 = document.getElementById("elemento-3")
+
+console.log(elemento3)
+
+const h2s = document.querySelectorAll("h2")
+
+console.log(h2s)
